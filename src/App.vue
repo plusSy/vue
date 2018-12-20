3404dbad599b823cc8708e74cd9938d2a74ef599
石一龙 <wb_shiyilong@ocj.com.cn>
2018-12-20 16:42:15 +0800

    补充提交

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <el-button @click="changeRoute('/router')">route</el-button>
-    <el-button @click="changeRoute('/router/a')">route/a</el-button>
-    <el-button @click="changeRoute('/router/b')">route/b</el-button>
-    <el-button @click="changeRoute('/')">home</el-button>
-    <el-button @click="changeRoute('/mock')">mock</el-button>
-    <el-button @click="changeRoute('/infinite')">infinite</el-button>
-    <el-button @click="changeRoute('/sort')">sort</el-button>
-    <el-button @click="changeRoute('/tableFilter')">tableFilter</el-button>
-    <router-view></router-view>
+    <div class="fl slide-bar">
+      <h1>route</h1>
+      <el-button class="define-class" v-for="(item, index) in routeConfig" :key="index" @click="changeRoute(`/${item.link}`)">{{item.name}}</el-button>
+    </div>
+    <div class="fr">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data () {
+    return {
+      routeConfig: [
+        {link: 'router', name: 'route'},
+        {link: 'router/a', name: 'router/a'},
+        {link: 'router/b', name: 'router/b'},
+        {link: '', name: 'home'},
+        {link: 'mock', name: 'mock'},
+        {link: 'sort', name: 'sort'},
+        {link: 'infinite', name: 'infinite'},
+        {link: 'tableFilter', name: 'tableFilter'}
+      ]
+    }
+  },
   methods: {
     changeRoute (router) {
       this.$router.push({path: router, query: {t: +new Date()}})
@@ -36,5 +48,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   font-size: 12px;
+  height: 100vh;
+}
+.fl{
+  float: left;
+  width: 10%;
+  height: 100%;
+}
+.fr{
+  float: right;
+  width: 89%;
+  height: 100%;
+}
+.slide-bar{
+  border-right: 1px solid #000;
+  text-align: center;
+}
+.define-class{
+  margin-bottom: 10px;
+  display: block;
 }
 </style>
--- src/views/tableFilter/index.vue
+++ src/views/tableFilter/index.vue
@@ -23,14 +23,14 @@ export default {
     return {
       list: [],
       baseData: [
-        {date: '2018年12月20日', name: '111111111111111', address: '上海市杨浦区'},
-        {date: '2018年12月21日', name: '1111112221', address: '上海市杨浦区'},
-        {date: '2018年12月22日', name: '1111133331111', address: '上海市杨浦区'},
-        {date: '2018年12月23日', name: '1114444111111', address: '上海市杨浦区'},
-        {date: '2018年12月24日', name: '333333', address: '上海市杨浦区'},
-        {date: '2018年12月25日', name: '555555555555555555', address: '上海市杨浦区'},
-        {date: '2018年12月26日', name: '66666666666666666666', address: '上海市杨浦区'},
-        {date: '2018年12月27日', name: '555555555544444444444433333333333', address: '上海市杨浦区'}
+        {date: '2018年12月20日', name: '111111111111111', address: '上海市杨浦区', isChecked: false},
+        {date: '2018年12月21日', name: '1111112221', address: '上海市杨浦区', isChecked: true},
+        {date: '2018年12月22日', name: '1111133331111', address: '上海市杨浦区', isChecked: true},
+        {date: '2018年12月23日', name: '1114444111111', address: '上海市杨浦区', isChecked: false},
+        {date: '2018年12月24日', name: '333333', address: '上海市杨浦区', isChecked: false},
+        {date: '2018年12月25日', name: '555555555555555555', address: '上海市杨浦区', isChecked: false},
+        {date: '2018年12月26日', name: '66666666666666666666', address: '上海市杨浦区', isChecked: true},
+        {date: '2018年12月27日', name: '555555555544444444444433333333333', address: '上海市杨浦区', isChecked: false}
       ],
       selectKey: '',
       multipleSelection: []
@@ -38,6 +38,9 @@ export default {
   },
   created () {
     this.list = this.baseData
+  },
+  mounted () {
+    this.setSelectStatus()
   },
   methods: {
     // change事件调用
@@ -60,18 +63,29 @@ export default {
           }
         })
       })
-
       setTimeout(() => {
         this.setSelectStatus(newArr.length > 0 ? newArr : multipleSelection)
       }, 0)
     },
     // 设置 勾选状态
-    setSelectStatus (arr) {
-      arr.map(row => {
-        this.$refs.multipleTable.toggleRowSelection(row)
+    setSelectStatus () {
+      let { baseData } = this
+      console.log('状态序列', baseData)
+      baseData.map(row => {
+        if (row.isChecked) {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        }
       })
     },
     handleSelectionChange (val) {
+      let { baseData } = this
+      baseData = baseData.map(item => {
+        val.map(cell => {
+          item.isChecked = item.name === cell.name ? true : item.isChecked
+        })
+        return item
+      })
+      this.baseData = baseData
       this.multipleSelection = val
     }
   }
